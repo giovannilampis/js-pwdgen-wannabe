c8993b998599224a7a4971386ed06d3a457fd7de
giovannilampis <giolampis@hotmail.com>
2023-03-21 15:00:17 +0100

    refactoring on script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-// ask the user what his / her name is
+// ask the user what his / her name is and make a variable out of it
 
 const nameUser = prompt("Qual è il tuo nome ?");
 
-// ask the user what his / her surname is
+// ask the user what his / her surname is and make a variable out of it
 
 const surnameUser = prompt("Qual è invece il tuo cognome ?");
 
-// ask the user what his / her favorite color is
+// ask the user what his / her favorite color is and make a variable out of it
 
 const favoriteColor = prompt("Qual è il tuo colore preferito ?");
 
-// write on the web page : nomecognomecolorepreferito21
+// password variable
 
 let password = `${nameUser}${surnameUser}${favoriteColor}21`;
 
